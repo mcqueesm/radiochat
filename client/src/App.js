5068156894e4b96f2e1ee5100cb6df36fd1c6db4
Sean McQueen <RoseSamuel@Seans-MacBook-Pro.local>
2019-03-30 15:51:13 -0700

    Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,233 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Alert, Button, InputGroup, InputGroupText, InputGroupAddon,
+    Collapse, CardBody, Card, Input, Form, Navbar,
+    NavbarToggler, NavbarBrand, Nav, NavItem, NavLink,
+    Dropdown, DropdownMenu, DropdownToggle, Modal, ModalHeader,
+    ModalBody, ModalFooter} from 'reactstrap';
+import * as io from 'socket.io-client';
+
+const socket = io.connect('http://localhost:5000');
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      messages: [],
+      input: '',
+      id: '',
+      name: '',
+      userNames: [],
+      localUsers: [],
+      newName:'',
+      location: {latitude: '', longitude: ''},
+      locationInterval: null,
+      collapse: false,
+      dropdownOpen: false,
+      modal: false,
+      nameSuccess: true
+
+    };
+    this.handleSend = this.handleSend.bind(this);
+    this.beginLocationEmit = this.beginLocationEmit.bind(this);
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.toggle = this.toggle.bind(this);
+    this.modalToggle = this.modalToggle.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
+    this.setName = this.setName.bind(this);
+  }
+  componentWillUpdate(){
+    var elem = document.getElementById('message-window');
+    elem.scrollTop = elem.scrollHeight;
+  }
+  componentDidMount() {
+    let current = this;
+    /*socket.on('chat', function(msg){
+      current.setState({messages: [...current.state.messages, msg]});
+    });*/
+
+    socket.on('on_connection', function(userInfo){
+      console.log("Client connecting")
+      current.setState({id: userInfo.id}, () => {
+        current.beginLocationEmit();
+        current.setName(userInfo.name);
+      });
+    });
+
+    socket.on('name_update', function(names){
+      current.setState({userNames: names}, () => {console.log("Received these names: ",  names)});
+    });
+
+    socket.on('change_name_result', function(result){
+
+      if (!result.success){
+        current.setState({nameSuccess: result.success}, ()=>{console.log(current.state.nameSuccess)});
+      }
+      else{
+        current.setState({name: result.name}, () => {console.log("My name changed to ", current.state.name)});
+      }
+    });
+
+    socket.on('locals', function(localNames){
+      let noLonger = current.state.localUsers.filter(x => !localNames.includes(x));
+      let newUsers = localNames.filter(x => !current.state.localUsers.includes(x));
+      console.log('No longer: ', noLonger);
+      console.log('New users: ', newUsers);
+      current.setState({localUsers: localNames});
+      if (noLonger){
+        noLonger.forEach(x => {
+          socket.off(x);
+        });
+      if (newUsers){
+        newUsers.forEach(x => {
+          socket.on(x, function(msg){
+            current.setState({messages: [...current.state.messages, msg]});
+          });
+        });
+      }
+      }
+      console.log("These are my local users: ", current.state.localUsers);
+    });
+
+  }
+  setName(name) {
+    let current = this;
+    current.setState({name: name}) /*, () =>{
+      socket.on(name, function(msg){
+        current.setState({messages: [...current.state.messages, msg]});
+      });
+    })*/
+
+  }
+
+  beginLocationEmit() {
+    let current = this;
+    this.setState({locationInterval: setInterval(function(){
+      socket.emit('location_update', {id: current.state.id, location: current.state.location});
+    }, 3000
+
+    )});
+  }
+  handleSend(event) {
+    event.preventDefault();
+    //socket.emit('chat', this.state.input);
+    this.setState({input: ''});
+    socket.emit(this.state.name, this.state.input);
+
+  }
+  toggle() {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen
+    });
+  }
+  toggleNavbar() {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
+  modalToggle() {
+    this.setState({modal: !this.state.modal});
+  }
+  handleNameChange(event) {
+    event.preventDefault();
+    socket.emit('change_name', this.state.newName);
+    this.setState({newName: '', modal: !this.state.modal});
+  }
+  onDismiss() {
+    this.setState({ nameSuccess: true });
+  }
+render() {
+  let current = this;
+  if ("geolocation" in navigator){
+    navigator.geolocation.getCurrentPosition(function(position) {
+      current.setState({location: {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+        }
+      });
+    });
+  }
+    let messageList = this.state.messages.map((x, index) => {
+      return (
+        <div id="message-item" key={index}> {x} </div>
+      );
+    });
+
+    let nameList = this.state.localUsers.map((x, index) => {
+      return (
+        <div id="name-item" key={index}> {x} </div>
+      );
+    });
+
+    return (
+      <div id="container">
+      <div>
+      <Navbar color="faded" light>
+        <NavbarBrand href="/" className="mr-auto">RadioChat</NavbarBrand>
+        <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+        <Collapse isOpen={this.state.collapsed} navbar>
+          <Nav navbar>
+            <NavItem>
+              <Dropdown isOpen={this.state.dropdownOpen} toggle={this.modalToggle}>
+                <DropdownToggle
+                tag="span"
+                onClick={this.modalToggle}
+                data-toggle="dropdown"
+                aria-expanded={this.state.dropdownOpen}
+                >
+                Change Name
+                </DropdownToggle>
+                <DropdownMenu>
+                  <Button color="danger" onClick={this.modalToggle}>Hello</Button>
+                  <Modal isOpen={this.state.modal} toggle={this.modalToggle} className={this.props.className}>
+                    <ModalHeader toggle={this.modalToggle}>Enter new name:</ModalHeader>
+                    <ModalBody>
+                      <Form>
+                        <InputGroup id='nameInput'>
+                          <Input value={this.state.newName} onChange={e=>this.setState({newName: e.target.value})}/>
+                          <InputGroupAddon addonType="append"><Button type='submit' onClick={this.handleNameChange}
+                            color="secondary">Submit</Button>
+                          </InputGroupAddon>
+                        </InputGroup>
+                      </Form>
+                    </ModalBody>
+
+                  </Modal>
+
+                </DropdownMenu>
+              </Dropdown>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+    {!this.state.nameSuccess ? (
+      <Alert color="danger" isOpen={!this.state.nameSuccess} toggle={this.onDismiss}>That name is already in use!
+      </Alert>) : <div></div>}
+        <div id="message-container">
+          <div id="message-window">
+            {messageList}
+          </div>
+          <div id="user-list">
+            {nameList}
+          </div>
+          <Form>
+            <InputGroup id='textInput'>
+              <Input id='textField' value={this.state.input} onChange={e=>this.setState({input: e.target.value})}/>
+              <InputGroupAddon addonType="append"><Button type='submit' onClick={this.handleSend}
+                color="secondary">Send</Button>
+              </InputGroupAddon>
+            </InputGroup>
+          </Form>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ socketBase.js
@@ -0,0 +1,117 @@
+const uuid = require('uuid');
+let guestNum = 1;
+let currentUsers = {};
+let currentNames = [];
+
+module.exports = function(io){
+  io.on('connection', function(socket){
+    console.log('CONNECTED!');
+
+    guestNum = assignName(socket, guestNum, currentUsers, currentNames);
+
+
+    socket.on('change_name', function(newName){
+      let result = {};
+      let oldName = currentUsers[socket.id].name;
+      if (currentNames.includes(newName)){
+        result = {success: false};
+      }
+      else{
+
+        changeName(socket, newName, currentUsers, currentNames);
+        result = {success: true, name: newName};
+        createUserListeners(socket, [newName]);
+
+      }
+
+      socket.emit('change_name_result', result);
+      io.emit('name_update', currentNames);
+      //io.emit('chat', oldName + ' has changed name to ' + newName);
+
+    });
+
+    io.emit('name_update', currentNames);
+    console.log('connected!');
+
+
+    /*socket.on('chat', function(msg) {
+      let message = currentUsers[socket.id].name + ': ' + msg;
+      io.emit('chat', message);
+    });*/
+
+    socket.on('location_update', function(clientUser){
+
+      currentUsers[clientUser.id].location = clientUser.location;
+      let localNames = [];
+      let lat1 = clientUser.location.latitude;
+      let lon1 = clientUser.location.longitude;
+      for (var user in currentUsers){
+
+        let lat2 = currentUsers[user].location.latitude;
+        let lon2 = currentUsers[user].location.longitude;
+        if(distance(lat1, lon1, lat2, lon2) <= 1){
+          localNames.push(currentUsers[user].name);
+        }
+      }
+      console.log('Current Users: ', currentUsers);
+      console.log('Current Names: ', currentNames);
+      socket.emit('locals', localNames);
+    });
+    socket.on('disconnect', function(){
+      console.log('DISCONNECTED!');
+
+      handleDisconnect(socket, currentUsers, currentNames);
+      io.emit('name_update', currentNames);
+    })
+
+    createUserListeners(socket, currentNames);
+  });
+  function assignName(socket, num, users, names){
+    let userId = socket.id;
+    let userObj = {
+      id: userId,
+      name: "Guest" + num,
+      location: {latitude: 0, longitude: 0}
+    };
+    names.push(userObj.name);
+    let userInfo = {userId: userObj};
+    users[userId] = userObj;
+    socket.emit('on_connection', userObj);
+    return ++num;
+  }
+  function handleDisconnect(socket, users, names){
+    let index = names.indexOf(users[socket.id].name);
+    delete names[index];
+    delete users[socket.id];
+  }
+  function changeName(socket, newName, users, names){
+    let index = names.indexOf(users[socket.id].name);
+    names[index] = newName;
+    users[socket.id].name = newName;
+  }
+  function createUserListeners(socket, names){
+    names.forEach(name => {
+      socket.on(name, function(msg){
+        let message = name + ': ' + msg;
+        io.emit(name, message);
+      });
+    });
+    console.log('Server events: ', socket.eventNames());
+  }
+  function distance(lat1, lon1, lat2, lon2, unit) {
+        var radlat1 = Math.PI * lat1/180;
+        var radlat2 = Math.PI * lat2/180;
+        var radlon1 = Math.PI * lon1/180;
+        var radlon2 = Math.PI * lon2/180;
+        var theta = lon1-lon2;
+        var radtheta = Math.PI * theta/180;
+        var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+        dist = Math.acos(dist);
+        dist = dist * 180/Math.PI;
+        dist = dist * 60 * 1.1515;
+        if (unit=="K") { dist = dist * 1.609344 }
+        if (unit=="N") { dist = dist * 0.8684 }
+        console.log("Distance: ", dist);
+        return dist;
+}
+}
