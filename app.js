70afb7b3cb9ec1b511bad25b2f09294d73566d2b
Sean McQueen <RoseSamuel@Seans-MacBook-Pro.local>
2019-04-25 17:48:34 -0700

    Changed package.json and app.js for heroku deployment

--- app.js
+++ app.js
@@ -1,46 +1,53 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mongoose = require('mongoose');
-var apiRouter = require('./routes/api');
-var config = require('config');
-
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var apiRouter = require("./routes/api");
+var config = require("config");
 
 var app = express();
-var socket_io = require( "socket.io" );
+var socket_io = require("socket.io");
 var io = socket_io();
 app.io = io;
 
 // socket.io events
-require('./socketBase.js')(io);
+require("./socketBase.js")(io);
 
 //mongooseURI
-var db = config.get('mongoURI');
+var db = config.get("mongoURI");
 // Connect to mongoose
 mongoose
-  .connect(db, {
-    useNewUrlParser: true,
-    useCreateIndex: true
-  }) // Adding new mongo url parser
-  .then(() => console.log('MongoDB Connected...'))
-.catch(err => console.log(err));
+  .connect(
+    db,
+    {
+      useNewUrlParser: true,
+      useCreateIndex: true
+    }
+  ) // Adding new mongo url parser
+  .then(() => console.log("MongoDB Connected..."))
+  .catch(err => console.log(err));
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api', apiRouter);
+app.use("/api", apiRouter);
 
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static(path.join(__dirname, "client", "build")));
 
-
-
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -51,11 +58,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
